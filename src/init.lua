ac91a6eb3e743ca3fbdedab5d28d8572b073af38
Bengejd <bengejd@miamioh.edu>
2021-06-03 17:26:12 -0500

    Clean: Project Structure

--- src/init.lua
+++ src/init.lua
@@ -1,36 +1,15 @@
---[[
--- This is the first file initialized in the addon, it's really just a global setter, if anything.
--- It can be mostly left empty
- ]]
-
-local _, core = ...;
+local _, PDKP = ...;
 local _G = _G;
-local L = core.L;
+local L = PDKP.L;
 
-core.PDKP = {}; -- global addon object
-core.GUI = {};
-core.DKP = {};
-core.Settings = {};
-core.Guild = {};
-core.Util = {};
-core.Character = {};
-core.Raid = {};
-core.Defaults = {};
-core.Member = {};
-core.Setup = {};
-core.Loot = {};
-core.Shroud = {};
-core.Comms = {};
-core.ScrollTable = {};
-core.DKP_Entry = {};
-core.HistoryTable = {};
-core.SimpleScrollFrame = {};
-core.Minimap = {};
-core.Export = {};
-core.Import = {};
+local LibStub = LibStub
+PDKP = LibStub("AceAddon-3.0"):NewAddon("PDKP", "AceConsole-3.0", "AceComm-3.0", "AceSerializer-3.0", "AceTimer-3.0")
+PDKP.PDKP.ldb = LibStub:GetLibrary("LibDataBroker-1.1")
+PDKP.PDKP.cbh = LibStub("CallbackHandler-1.0"):New(PDKP)
+PDKP.LibDeflate = LibStub:GetLibrary("LibDeflate")
 
-core.PDKP = LibStub("AceAddon-3.0"):NewAddon("PDKP", "AceConsole-3.0", "AceComm-3.0", "AceSerializer-3.0", "AceTimer-3.0");
-core.PDKP.ldb = LibStub:GetLibrary("LibDataBroker-1.1")
-core.PDKP.cbh = LibStub("CallbackHandler-1.0"):New(core.PDKP);
-core.LibDeflate = LibStub:GetLibrary("LibDeflate")
+_G.PDKP = PDKP
 
+PDKP.GUI, PDKP.DKP, PDKP.Settings, PDKP.Guild, PDKP.Util, PDKP.Character, PDKP.Raid, PDKP.Defaults, PDKP.Member,
+PDKP.Setup, PDKP.Loot, PDKP.Shroud, PDKP.Comms, PDKP.ScrollTable, PDKP.DKP_Entry, PDKP.HistoryTable,
+PDKP.SimpleScrollFrame, PDKP.Minimap, PDKP.Export, PDKP.Import = {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {};
