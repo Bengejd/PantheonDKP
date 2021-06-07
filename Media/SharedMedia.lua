e8e1644d745d8df3d59826d9731a723f5ea2c55f
Bengejd <bengejd@miamioh.edu>
2021-06-07 09:55:33 -0500

    Update: SharedMedia.lua

--- Media/SharedMedia.lua
+++ Media/SharedMedia.lua
@@ -1,16 +1,17 @@
 local _, core = ...;
 local _G = _G;
 
-local Media = core.PDKP.Media
+local Media = core.PDKP:GetInst('Media')
 
 Media.CLOSE_BUTTON_TEXT = "|TInterface\\Buttons\\UI-StopButton:0|t"
 Media.TRANSPARENT_BACKGROUND = "Interface\\TutorialFrame\\TutorialFrameBackground"
-Media.PDKP_TEXTURE_BASE = "Interface\\Addons\\PantheonDKP\\Media\\Main_UI\\PDKPFrame-"
+Media.PDKP_TEXTURE_BASE = "Interface\\Addons\\PantheonDKP\\Media\\New_UI\\PDKPFrame-"
+--Media.PDKP_TEXTURE_BASE = "Interface\\Addons\\PantheonDKP\\Media\\New_UI\\PDKPFrame-"
 Media.SHROUD_BORDER = "Interface\\DialogFrame\\UI-DialogBox-Border"
 Media.SCROLL_BORDER = "Interface\\Tooltips\\UI-Tooltip-Border"
 Media.CHAR_INFO_TEXTURE = 'Interface\\CastingBar\\UI-CastingBar-Border-Small'
 
-Media.adoon_version_hex = '0059C5'
+Media.addon_version_hex = '0059C5'
 
 Media.PaneBackdrop  = {
     bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
