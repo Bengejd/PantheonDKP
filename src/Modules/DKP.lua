6812c6bde9014f13e0475d6f6f3ac70190325c45
Bengejd <bengejd@miamioh.edu>
2021-06-08 15:12:51 -0500

    Update DKP.lua

--- src/Modules/DKP.lua
+++ src/Modules/DKP.lua
@@ -1,6 +1,8 @@
 local _G = _G;
 local PDKP = _G.PDKP;
 
--- [[
---    this is my comment
+--[[
+    DKP Entries could possibly follow an "Added" and "Spent" Index Key, to keep track of when you spent DKP vs when you added DKP.
+    DKP "Added" entries, should also denote whether you were raiding, or on standby for a raid.
+    Potentially list killed DKP bosses, and when you are available to receive new DKP for them.
 --]]
