7076e221eb64137dedb22677a4cd9001ec6cb5fc
Bengejd <bengejd@miamioh.edu>
2021-06-18 01:04:02 -0500

    Update GUtils.lua

--- Modules/Utils/GUtils.lua
+++ Modules/Utils/GUtils.lua
@@ -11,7 +11,9 @@ local Utils = PDKP.Utils;
 local GUtils = PDKP.GUtils
 
 function GUtils:setMovable(f)
-    if f == nil then return end
+    if f == nil then
+        return
+    end
     f:SetMovable(true)
     f:EnableMouse(true)
     f:RegisterForDrag('LeftButton')
@@ -61,7 +63,9 @@ function GUtils:createCheckButton(opts)
 end
 
 function GUtils:createBackdropFrame(name, parent, title)
-    if parent == nil then return end
+    if parent == nil then
+        return
+    end
     title = title or ''
     name = name or nil
 
@@ -95,11 +99,14 @@ function GUtils:createBackdropFrame(name, parent, title)
     f.border = border
     f.content = content
     f.title = title_text;
+    f.children = {};
     return f
 end
 
 function GUtils:createIcon(parent, label_text)
-    if parent == nil then return end
+    if parent == nil then
+        return
+    end
     label_text = label_text or ''
 
     local f = CreateFrame("Frame", nil, parent)
@@ -137,9 +144,12 @@ function GUtils:createDropdown(opts)
     local default_val = opts['defaultVal'] or ''
     local hide = opts['hide'] or false
 
-    local showFunc = opts['showFunc'] or function() end
-    local hideFunc = opts['hideFunc'] or function() end
-    local changeFunc = opts['changeFunc'] or function() end
+    local showFunc = opts['showFunc'] or function()
+    end
+    local hideFunc = opts['hideFunc'] or function()
+    end
+    local changeFunc = opts['changeFunc'] or function()
+    end
 
     local dropdown = CreateFrame("Frame", dropdown_name, opts['parent'], 'UIDropDownMenuTemplate')
     local dd_title = dropdown:CreateFontString(dropdown, 'OVERLAY', 'GameFontNormal')
@@ -158,7 +168,9 @@ function GUtils:createDropdown(opts)
     local itemsCopy = PDKP.Utils.ShallowCopy(menu_items)
 
     -- Sort the items table by length
-    table.sort(itemsCopy, function(a, b) return string.len(a) > string.len(b) end)
+    table.sort(itemsCopy, function(a, b)
+        return string.len(a) > string.len(b)
+    end)
 
     -- Grab the longest string from the sorted table
     local longest_item = itemsCopy[1]
@@ -182,7 +194,9 @@ function GUtils:createDropdown(opts)
         return dropdown:IsVisible() and box_text ~= nil and box_text ~= "" and box_text ~= 0;
     end
 
-    if hide then dropdown:Hide() end
+    if hide then
+        dropdown:Hide()
+    end
 
     -- We need to define the initialize function via blizzard's method.
     UIDropDownMenu_Initialize(dropdown, function()
@@ -209,8 +223,139 @@ function GUtils:createDropdown(opts)
     end)
 
     -- Attaches this to a global object, if provided.
-    if opts['dropdownTable'] then dropdown.dropdownTable = opts['dropdownTable'] end
-    Utils:WatchVar(dropdown, dropdown.uniqueID)
+    if opts['dropdownTable'] then
+        dropdown.dropdownTable = opts['dropdownTable']
+    end
+
+    return dropdown
+end
+
+--- Opts:
+---     name (string): Name of the dropdown (lowercase)
+---     parent (Frame): Parent frame of the dropdown.
+---     items (Table): table of the dropdown options & their sub-menus.
+---     defaultVal (String): String value for the dropdown to default to (empty otherwise).
+---     hide (Boolean): A boolean value for whether the dropdown should start hidden.
+---     dropdownTable (table): A table of dropdowns for this to be inserted into.
+---     showOnValue (string): A custom value for when the table should be shown.
+---     changeFunc (Function): A custom function to be called, after selecting a dropdown option.
+---     showFunc (Function): A custom function to be called, when the dropdown shows.
+---     hideFunc (Function): A custom function to be called, when the dropdown hides.
+function GUtils:createNestedDropdown(opts)
+    local dropdown_name = '$parent_' .. opts['name'] .. '_dropdown'
+    local menu_items = opts['items'] or {}
+    local title_text = opts['title'] or ''
+    local default_val = opts['defaultVal'] or ''
+    local hide = opts['hide'] or false
+
+    local showFunc = opts['showFunc'] or function()
+    end
+    local hideFunc = opts['hideFunc'] or function()
+    end
+    local changeFunc = opts['changeFunc'] or function()
+    end
+
+    local dropdown = CreateFrame("Frame", dropdown_name, opts['parent'], 'UIDropDownMenuTemplate')
+
+    local dd_title = dropdown:CreateFontString(dropdown, 'OVERLAY', 'GameFontNormal')
+    dd_title:SetPoint("TOPLEFT", 20, 10)
+
+    dropdown.uniqueID = opts['name']
+    dropdown.showOnValue = opts['showOnValue'] or 'Always'
+    dropdown.initialized = false -- Used later on, in initialization
+    dropdown.children = opts['children'] or {}
+
+    --- BUG FIX Start: Menu Width
+    --- Menu width doesn't dynamically change, at least not easily. To get around this, we find the longest string, pop
+    --- it into the dd_title font string, calculate the width with some padding, and set that as our dropdown width.
+
+    -- ShallowCopy the menu_items so we don't accidentally sort the original table.
+    local itemsCopy = PDKP.Utils.ShallowCopy(menu_items)
+
+    local table_keys = {} -- We use a header vs button for separation.
+    for key, v in pairs(itemsCopy) do
+        table.insert(table_keys, key)
+    end
+
+    -- Sort the items table by length
+    table.sort(table_keys, function(a, b)
+        return string.len(a) > string.len(b)
+    end)
+
+    -- Grab the longest string from the sorted table
+    local longest_item = table_keys[1]
+
+    dd_title:SetText(longest_item);
+
+    local title_padding = 20;
+    local dropdown_width = dd_title:GetStringWidth() + title_padding
+
+    UIDropDownMenu_SetWidth(dropdown, dropdown_width)
+    UIDropDownMenu_SetText(dropdown, default_val)
+    dd_title:SetText(title_text)
+
+    --- End BUG FIX: Menu Width
+
+    -- Dropdown Functions
+    dropdown:SetScript("OnShow", showFunc)
+    dropdown:SetScript("OnHide", hideFunc)
+    dropdown.isValid = function()
+        local box_text = UIDropDownMenu_GetSelectedValue(dropdown)
+        return dropdown:IsVisible() and box_text ~= nil and box_text ~= "" and box_text ~= 0;
+    end
+
+    -- We need to define the initialize function via blizzard's method.
+    function PDKP_DropDown_Initialize(self,level)
+        level = level or 1;
+
+        if level == 1 then
+            for key, _ in pairs(menu_items) do
+                local info = UIDropDownMenu_CreateInfo();
+                info.hasArrow = true; -- creates submenu
+                info.notCheckable = true;
+                info.keepShownOnClick = false;
+                info.text = key;
+                info.value = {
+                    ["Level1_Key"] = key;
+                };
+                UIDropDownMenu_AddButton(info, level);
+            end
+        end
+
+        if level == 2 then
+            -- getting values of first menu
+            local Level1_Key = UIDROPDOWNMENU_MENU_VALUE["Level1_Key"];
+            local subarray = menu_items[Level1_Key];
+            for _, boss_name in pairs(subarray) do
+                local info = UIDropDownMenu_CreateInfo();
+                info.hasArrow = false; -- no submenues this time
+                info.notCheckable = false;
+                info.text = boss_name
+                info.keepShownOnClick = false;
+                info.value = boss_name;
+                info.func = function(b)
+                    UIDropDownMenu_SetSelectedValue(dropdown, b.value, b.value)
+                    UIDropDownMenu_SetText(dropdown, b.value)
+                    ToggleDropDownMenu(1, nil, dropdown)
+                    b.checked = true
+                    changeFunc(dropdown, dropdown, b)
+                end
+                UIDropDownMenu_AddButton(info, level);
+            end
+        end
+
+        if not dropdown.initialized and default_val and default_val ~= '' then
+            dropdown.initialized = true;
+            UIDropDownMenu_SetSelectedValue(dropdown, default_val, default_val)
+        end
+    end
+
+    UIDropDownMenu_Initialize(dropdown, PDKP_DropDown_Initialize, nil);
+
+    -- Attaches this to a global object, if provided.
+    if opts['dropdownTable'] then
+        dropdown.dropdownTable = opts['dropdownTable']
+    end
 
     return dropdown
 end
@@ -224,6 +369,8 @@ end
 ---     textValidFunc (Function)
 ---     numeric (int)
 ---     small_title (boolean)
+---     showOnValue (string)
+---     dropdownTable (table)
 ---
 function GUtils:createEditBox(opts)
     local name = opts['name'] or 'edit_box'
@@ -235,6 +382,8 @@ function GUtils:createEditBox(opts)
     end
     local numeric = opts['numeric'] or false
     local small_title = opts['smallTitle'] or false
+    local dropdown_table = opts['dropdownTable'] or nil
+    local showOnValue = opts['showOnValue'] or nil
 
     local box = CreateFrame("EditBox", "$parent_" .. name, parent)
     box:SetHeight(30)
@@ -246,27 +395,34 @@ function GUtils:createEditBox(opts)
     box:SetMultiLine(multi_line)
     box:SetNumeric(numeric)
     box.uniqueID = name
+    box.selectedValue = nil --- Ease of use for combining in dropdowns.
 
     box.touched = false
+
+    if opts['showOnValue'] then box.showOnValue = opts['showOnValue'] end
+    if opts['dropdownTable'] then box.dropdownTable = opts['dropdownTable'] end
 
     box.isValid = function()
         return box:IsVisible()
                 and (name == 'other'
                 or (box:IsNumeric() and box:GetNumber() > 0)
-                or (box:GetText() and box:GetText() ~= ""))
+                or (not box:IsNumeric() and box:GetText() and box:GetText() ~= ""))
     end
     box.getValue = function()
-        if numeric then return box:GetNumber() end
+        if numeric then
+            return box:GetNumber()
+        end
         return box:GetText()
     end
 
     box:SetScript("OnEscapePressed", function() box:ClearFocus() end)
     box:SetScript("OnTextChanged", function()
-        -- Todo: Need to refactor this...
-        if box.isValid() then textValidFunc() else textValidFunc() end
+        box.selectedValue = box.getValue()
+        textValidFunc()
     end)
-
-    box:SetScript("OnEditFocusGained", function() box.touched = true end)
+    box:SetScript("OnEditFocusGained", function()
+        box.touched = true
+    end)
 
     local box_frame = CreateFrame("Frame", '$parent_edit_frame', box, MODULES.Media.BackdropTemplate)
 
@@ -287,11 +443,15 @@ function GUtils:createEditBox(opts)
         box_frame:SetHeight(30)
     end
 
-    if box:GetFrameLevel() > 4 then box_frame:SetFrameLevel(box:GetFrameLevel() - 4) end
+    if box:GetFrameLevel() > 4 then
+        box_frame:SetFrameLevel(box:GetFrameLevel() - 4)
+    end
 
     local title_font = 'GameFontNormal'
 
-    if small_title then title_font = 'GameFontNormalSmall' end
+    if small_title then
+        title_font = 'GameFontNormalSmall'
+    end
 
     -- label
     local el = box:CreateFontString(box_frame, "OVERLAY", title_font)
@@ -352,11 +512,15 @@ function GUtils:createItemLink(parent)
         local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, _, _, itemStackCount, _, itemTexture,
         itemSellPrice = GetItemInfo(itemIdentifier)
 
-        if not itemLink then return end
+        if not itemLink then
+            return
+        end
 
         fs:SetText(itemLink)
         fs.iLink = itemLink
-        if fs.icon then fs.icon:SetTexture(itemTexture) end
+        if fs.icon then
+            fs.icon:SetTexture(itemTexture)
+        end
 
         return fs.iLink, itemTexture
     end
