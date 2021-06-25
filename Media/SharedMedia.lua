6a9ff64fd491bdbc3439bc70886be6ecdb77de3e
Bengejd <bengejd@miamioh.edu>
2021-06-25 01:13:30 -0500

    Add: DKP_Officer icon

--- Media/SharedMedia.lua
+++ Media/SharedMedia.lua
@@ -44,6 +44,7 @@ Media.TAB_TEXTURE = "Interface\\CHATFRAME\\ChatFrameTab"
 
 Media.TANK_ICON = 'Interface\\ICONS\\Ability_Defend'
 Media.TOTAL_ICON = 'Interface\\ICONS\\Achievement_GuildPerk_EverybodysFriend'
+Media.DKP_OFFICER_ICON = 'Interface\\ICONS\\INV_MISC_Coin_01'
 
 Media.addon_version_hex = '0059C5'
 
