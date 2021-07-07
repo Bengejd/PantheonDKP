29132a572bc07e4b36b8f5e6d63bcc22cea81e7b
Bengejd <bengejd@miamioh.edu>
2021-07-07 13:27:27 -0500

    Feat: Auto Reload after DB Reset

--- Modules/Database/Database.lua
+++ Modules/Database/Database.lua
@@ -2,8 +2,6 @@ local _, PDKP = ...
 
 local MODULES = PDKP.MODULES
 
-local ReloadUI = ReloadUI
-
 local DB = {}
 
 local database_names = { 'personal', 'guild', 'dkp', 'pug', 'officers', 'settings', 'lockouts', 'loot', 'ledger' }
@@ -90,11 +88,6 @@ function DB:ResetAllDatabases()
         local db = database_names[i]
         PDKP_DB[self.server_faction_guild][db] = {}
     end
-    PDKP.CORE:Print('Databases have been reset... reloading in 5 seconds');
-
-    C_Timer.After(5, function()
-        ReloadUI();
-    end)
 end
 
 function DB:ResetLockouts()
