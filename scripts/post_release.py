831dcbecee793cc4e39b3dc610986dfbe6f54115
Bengejd <bengejd@miamioh.edu>
2021-08-13 00:26:37 -0500

    Update post_release.py

--- scripts/post_release.py
+++ scripts/post_release.py
@@ -34,7 +34,7 @@ def get_newest_changelog():
         if response.status_code == 200:
             return response.json()
         else:
-            print("Query failed")
+            print("Changelog Query failed")
             if num_calls >= 10:
                 print("exiting")
                 exit(10)
